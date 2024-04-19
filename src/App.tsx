efb0bdb938f7015a8d3094d3bc2f3871c9d63506
belemaaa <graceitamunoala@gmail.com>
2024-04-19 12:49:23 +0100

    type declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import './App.css';
 
+
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+let printName: (name: string) => never;
+
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// }
+
+// let person: Person = {
+//   name: "Test",
+//   age: 12
+// }
+
+
 function App() {
   return (
     <div>
